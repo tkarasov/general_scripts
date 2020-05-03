65e9fb8629ecdffd0d995678060072bfc53e33e0
tkarasov <tkarasov@gmail.com>
2020-05-03 08:43:01 +0200

    Update run_picard.sh

--- run_picard.sh
+++ run_picard.sh
@@ -1,3 +1,32 @@
+#!/bin/bash
+#
+#
+#
+#####################################################################
+# Run picard to remove duplicates
+#
+# Usage: bash run_picard.sh <sample>
+# or:
+#       qsub -N sample -o plantRemoval.out -e plantRemoval.err run_plantRemoval.sh samplename read1.fq read.fq
+# where:
+#       sample = "Name of the sample to process, this string
+#                     will be used as the read group in the bamfile"
+#
+####################################################################
+
+#$ -cwd
+#
+#  Import environment
+#$ -V
+$
+# Request 12 cores
+#$ -pe parallel 12
+#
+# Request 8Gb per process
+#$ -l h_vmem=8G
+#
+
+
 # Run picardtools to remove PCR and optical duplicates from alignment
 # file
 java -Xmx20g -jar \
